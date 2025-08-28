b0b09aa3008dae84dc5c66c87eb004186104f5e2
Samarth Jaiswal <samjaiswal51@gmail.com>
2025-08-28 19:26:56 +0530

    user schema

---
+++ backend/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['patient', 'doctor', 'family', 'admin'],
+    default: 'patient',
+  },
+});
+
+// Password hashing middleware using bcrypt
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+// Method to compare password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
